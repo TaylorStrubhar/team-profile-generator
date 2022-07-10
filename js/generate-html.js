e159b502486b3a7008a2a1e2b68246ccca6e4191
Ira Strubhar <taylor.strubhar@gmail.com>
2022-07-09 21:42:52 -0700

    cleaning up generated html/css

--- js/generate-html.js
+++ js/generate-html.js
@@ -67,7 +67,6 @@ for (let i = 0; i < team.length; i++) {
 
 return html.join('');
 };
-// 72
 
 module.exports = team => {
 
@@ -80,7 +79,7 @@ module.exports = team => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/3aa9e9469d.js" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="../css/style.css" />
+        <link rel="stylesheet" href="../../css/style.css" />
         <title>Your Team Profile</title>
     </head>
     <body>
